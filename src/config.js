d5d4e41bb8e4ca7559ad8d4c01ea0e83ac06b074
Tisyanchin Vasya <tisyanchin13@gmail.com>
2017-07-21 18:53:41 +0300

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const config = {};
 
 
-if(process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     config.port = 3000;
     config.DBurl = 'mongodb://127.0.0.1:27017/FirstApp';
-} else {
+} else if (process.env.NODE_ENV === 'prod') {
     config.port = process.env.PORT;
     config.dbName = "firstappusers";
     config.dbHost = "Tvolex:Tvolex3913@ds031895.mlab.com";
