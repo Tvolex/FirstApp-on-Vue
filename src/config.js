324536f4e039883be80fdfb11c120c2142150994
Tisyanchin Vasya <tisyanchin13@gmail.com>
2017-07-24 17:12:05 +0300

    fix config.js

--- src/config.js
+++ src/config.js
@@ -2,10 +2,18 @@ const config = {};
 
 
 if (process.env.NODE_ENV === 'dev') {
-    config.port = 3000;
-    config.DBurl = 'mongodb://127.0.0.1:27017/FirstApp';
-} else if (process.env.NODE_ENV === 'prod') {
     config.port = process.env.PORT || 3000;
+    config.DBurl = 'mongodb://127.0.0.1:27017/FirstApp';
+}
+else if(process.env.NODE_ENV === 'DevDeploy') {
+    config.port = process.env.PORT;
+    config.dbName = "firstappusers";
+    config.dbHost = "Tvolex:Tvolex3913@ds031895.mlab.com";
+    config.dbPort = "31895";
+    config.DBurl = process.env.ENV ||`mongodb://${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
+else if (process.env.NODE_ENV === 'prod') {
+    config.port = process.env.PORT;
     config.dbName = "firstappusers";
     config.dbHost = "Tvolex:Tvolex3913@ds031895.mlab.com";
     config.dbPort = "31895";
