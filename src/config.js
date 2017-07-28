ac55309ff7f37bb16e42b0bdf8124c3bf81eab15
Tisyanchin Vasya <tisyanchin13@gmail.com>
2017-07-28 12:57:22 +0300

    update config.js

--- src/config.js
+++ src/config.js
@@ -7,10 +7,7 @@ if (process.env.NODE_ENV === 'dev') {
 }
 else if(process.env.NODE_ENV === 'DevDeploy') {
     config.port = process.env.PORT;
-    config.dbName = "firstappusers";
-    config.dbHost = "Tvolex:Tvolex3913@ds031895.mlab.com";
-    config.dbPort = "31895";
-    config.DBurl = process.env.ENV ||`mongodb://${config.dbHost}:${config.dbPort}/${config.dbName}`;
+    config.DBurl = `mongodb://FirstApp-dev:WxHIKP7QSX27vQN2@fisrtappgroup-shard-00-00-q3owb.mongodb.net:27017,fisrtappgroup-shard-00-01-q3owb.mongodb.net:27017,fisrtappgroup-shard-00-02-q3owb.mongodb.net:27017/FirstApp?ssl=true&replicaSet=FisrtAppGroup-shard-0&authSource=admin`;
 }
 else if (process.env.NODE_ENV === 'prod') {
     config.port = process.env.PORT;
